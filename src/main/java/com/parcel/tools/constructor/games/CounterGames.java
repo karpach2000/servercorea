3d52d2bb589e8abfc95d1c6cdf948770042ffc7a
tree <karpach2000@yandex.ru>
2020-04-23 15:39:04 +0300

    Добавил подсказки при игре в шпиона

--- src/main/java/com/parcel/tools/constructor/games/CounterGames.java
+++ src/main/java/com/parcel/tools/constructor/games/CounterGames.java
@@ -1,24 +1,23 @@
 package com.parcel.tools.constructor.games;
 
+import com.parcel.tools.constructor.games.spy.CounterGamesSpy;
 import com.parcel.tools.spy.SpySessionManager;
 import com.parcel.tools.spy.database.SpyLocation;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс который в дальнейшем серриализуется в WEB страницу при помощи шаблонизатора.
+ */
 public class CounterGames {
     public CounterMenuGames counterMenu= new CounterMenuGames();
     public String descriptionText="Игры.";
-    public List<SpyLocation> locations = new ArrayList<SpyLocation>();
+    public CounterGamesSpy counterGamesSpy =new CounterGamesSpy();
+
     public CounterGames()
     {
         counterMenu.addItem("Шпион.","ropeSlack");
-        try {
-            locations = SpySessionManager.INSTANCE.getLocationList();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+
     }
 }
---
+++ src/main/java/com/parcel/tools/constructor/games/spy/CounterGamesSpy.java
@@ -0,0 +1,28 @@
+package com.parcel.tools.constructor.games.spy;
+
+import com.parcel.tools.spy.SpySessionManager;
+import com.parcel.tools.spy.database.SpyLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CounterGamesSpy {
+    public List<SpyLocation> locations = new ArrayList<SpyLocation>();
+    public String gameDescription = "Описание правил и игры!";
+    public String tooltipWhenAddingUsers = "Подсказка при добавлении пользователей";
+    public String tooltipDuringTheGame = "Подсказка во время игры";
+
+
+
+    public CounterGamesSpy()
+    {
+        try {
+            locations = SpySessionManager.INSTANCE.getLocationList();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
