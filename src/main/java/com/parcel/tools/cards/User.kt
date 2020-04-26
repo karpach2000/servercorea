9e86b5b8e65a704a7746b1e9bd17b74eaf2b3a65
karpach2000 <karpach2000@yandex.ru>
2020-04-26 19:31:26 +0300

    Revert "Добавлены файлы Карточек"

--- src/main/java/com/parcel/tools/cards/User.kt
+++
@@ -1,6 +0,0 @@
-package com.parcel.tools.cards
-
-data class User(val name: String) {
-    var userCard: String = ""
-    var wantSeeCards = false
-}
--- src/main/java/com/parcel/tools/spy/database/SpyLocation.kt
+++
@@ -1,9 +0,0 @@
-package com.parcel.tools.spy.database
-
-class SpyLocation {
-    var id : Int?=null
-    var locationName = ""
-    var authorLogin = ""
-
-
-}
