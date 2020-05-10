dd464177a520c6a294cff822872c18109b0a6409
Nikolaj <mikola911@gmail.com>
2020-05-10 18:21:46 +0300

    some fixes

--- src/main/resources/static/web/javascript/reg.js
+++ src/main/resources/static/web/javascript/reg.js
@@ -5,10 +5,22 @@ function reg() {
     var login = document.getElementById("login").value
     var password = document.getElementById("password").value
     var password2 = document.getElementById("password2").value
-    xmlHttp.open("GET", "/users/reg/addUserU?login="+login+"&password="+password+
+
+    let checkUsername = checkUser(login);
+    if (checkUsername==false) {
+        //alert - имя существует
+    } else if (password != password2){
+        //alert - пароли не совпадают
+    } else {
+        // отправляем на сервер
+        xmlHttp.open("GET", "/users/reg/addUserU?login="+login+"&password="+password+
         "&password2="+password2, false); // false for synchronous request
-    xmlHttp.send(null);
+        xmlHttp.send(null);
+        // alert(xmlHttp.responseText)
+        document.getElementById("alertMessage").innerHTML = xmlHttp.responseText ;
+        $('#alertModal').modal(show)
+    }
+    
     //console.log("GET RX: " + xmlHttp.responseText)
-    // alert(xmlHttp.responseText)
-    document.getElementById("alertMessage").innerHTML = xmlHttp.responseText ;
+
 }
