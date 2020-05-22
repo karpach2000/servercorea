793b0329504159ced1199e0e9331125a1a80c43b
karpach2000 <karpach2000@yandex.ru>
2020-05-22 17:44:56 +0300

    Добавил соединение с базами данных

--- src/main/java/com/parcel/tools/Globals.kt
+++ src/main/java/com/parcel/tools/Globals.kt
@@ -2,9 +2,11 @@ package com.parcel.tools
 
 import com.parcel.tools.constructor.database.users.UserManager
 import com.parcel.tools.games.games.spy.database.SpyLocationManager
+import com.parcel.tools.games.games.thirtyyears.database.ThirtyYearsEventManager
 
 object Globals {
 
     lateinit var userManager: UserManager
     lateinit var spyLocationManager: SpyLocationManager
+    lateinit var thirtyYearsEventManager: ThirtyYearsEventManager
 }
---
+++ src/main/java/com/parcel/tools/games/games/thirtyyears/TirtyYearsSession.kt
@@ -0,0 +1,4 @@
+package com.parcel.tools.games.games.thirtyyears
+
+class TirtyYearsSession {
+}
